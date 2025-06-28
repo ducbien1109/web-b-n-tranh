e54a3398052d7100a23f2042b7b4f0981ebb3a9a
ducbien1109 <ducbien911@gmail.com>
2025-06-28 22:38:37 +0700

    Cập nhật code

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@
 // });
 // app.listen(5000, () => console.log("Server running on http://localhost:5000"));
 // //
-
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -86,11 +86,17 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 // Kết nối MongoDB
+// mongoose
+//   .connect("mongodb://localhost:27017/productdb", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
 mongoose
-  .connect("mongodb://localhost:27017/productdb", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
@@ -211,10 +217,11 @@ app.get("/users", async (req, res) => {
     res.status(500).json({ message: "Lỗi khi lấy người dùng" });
   }
 });
+//
 
 // ------------------------------------------------- //
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
